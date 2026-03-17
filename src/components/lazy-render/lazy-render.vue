6034283bc9a84b19eecad166fa67395e9ee8d424
zcc <350018736@qq.com>
2026-03-17 17:26:17 +0800

    图片懒加载组件修改

--- src/components/lazy-render/lazy-render.vue
+++ src/components/lazy-render/lazy-render.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="lazy-observer-root">
+  <view :class="uniqueClass">
     <slot :visible="visible" />
   </view>
 </template>
@@ -7,7 +7,10 @@
 <script setup lang="ts">
 import { getCurrentInstance, onMounted, onUnmounted, ref } from 'vue'
 
+const instance = getCurrentInstance()
+
 const visible = ref(false)
+const uniqueClass = ref(`lazy-observer-${instance?.uid}`)
 
 let observer: UniApp.IntersectionObserver | null = null
 
@@ -16,27 +19,27 @@ const props = withDefaults(
     bottom?: number
   }>(),
   {
-    bottom: 200
+    bottom: 0
   }
 )
 
 onMounted(() => {
   // #ifdef H5
-  const instance = getCurrentInstance()
-
   observer = uni.createIntersectionObserver(instance?.proxy, {
-    thresholds: [0]
+    thresholds: [0.01]
   })
 
   // 以当前组件根节点作为观察目标
-  observer.relativeToViewport({ bottom: props.bottom }).observe('.lazy-observer-root', (result) => {
-    if (result.intersectionRatio > 0) {
-      visible.value = true
-      // 一旦进入视口就不再继续监听（一次性懒加载）
-      observer?.disconnect()
-      observer = null
-    }
-  })
+  observer
+    .relativeToViewport({ bottom: props.bottom })
+    .observe(`.${uniqueClass.value}`, (result) => {
+      if (result.intersectionRatio > 0) {
+        visible.value = true
+        // 一旦进入视口就不再继续监听（一次性懒加载）
+        observer?.disconnect()
+        observer = null
+      }
+    })
   // #endif
 })
 
